34170fe20e1d240a0c7fd01233989eb2f538968d
Peizhen Liao <peizhenliao23@gmail.com>
2024-06-25 22:41:33 -0400

    initiate

---
+++ kafka.tf
@@ -0,0 +1,24 @@
+resource "helm_release" "kafka" {
+  name       = "kafka"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "kafka"
+  namespace  = kubernetes_namespace.kafka.metadata[0].name
+
+  values = [
+    <<EOF
+        replicaCount: 3
+        zookeeper:
+        replicaCount: 3
+        resources:
+        requests:
+            memory: "1Gi"
+            cpu: "500m"
+        limits:
+            memory: "2Gi"
+            cpu: "1"
+        EOF
+  ]
+
+  # depends_on =  [kubernetes_namespace.kafka]
+  depends_on = [module.eks, kubernetes_namespace.kafka]
+}
